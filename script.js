fb6e1ad8fb9a48e9c91d2f3650edd7bdcb3e1b41
heyvartika24-glitch <heyvartika24@gmail.com>
2026-08-01 21:54:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let num1 = 5;
+let num2 = 8;
+let num3 = 11;
+let num4 = 20;
+
+if (num1 % 2 == 0) {
+  console.log("The number", num1, "is Even.");
+} else {
+  console.log("The number", num1, "is Odd.");
+}
+
+if (num2 % 2 == 0) {
+  console.log("The number", num2, "is Even.");
+} else {
+  console.log("The number", num2, "is Odd.");
+}
+
+if (num3 % 2 == 0) {
+  console.log("The number", num3, "is Even.");
+} else {
+  console.log("The number", num3, "is Odd.");
+}
+
+if (num4 % 2 == 0) {
+  console.log("The number", num4, "is Even.");
+} else {
+  console.log("The number", num4, "is Odd.");
+}
